b796cab2476a854a39fa3222ab85c30d35c634e4
Huynhtri203 <150775394+Huynhtri203@users.noreply.github.com>
2023-11-14 14:02:42 +0700

    init

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'cluster0.ekzabi4.mongodb.net',
+    DB_USER: 'tritri21',
+    DB_PASS: 'minhtri123456',
+    DB_DATABASE: 'shoppingonline',
+    JWT_SECRET: 'MinhTri',
+    JWT_EXPIRES: '36000000', // in milliseconds
+    EMAIL_USER: 'huynhtri2192003@gmail.com', // gmail service
+    EMAIL_PASS: 'tvpz mach uhqt igsg'
+  };
+  module.exports = MyConstants;
